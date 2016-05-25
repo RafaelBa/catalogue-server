0179517d27915d1fc3b5fddfbcc35dc2377ca289
RafaelBa <rafael.balski@gopopup.com>
2016-05-25 16:54:05 +0200

    Service reads from Zalando API

---
+++ app/net/codealchemist/sparks42/catalogue/Representation.scala
@@ -0,0 +1,15 @@
+package net.codealchemist.sparks42.catalogue
+
+import play.api.libs.functional.syntax._
+import play.api.libs.json.{Reads, __}
+
+object Represenation {
+  case class Article(name: String, brand: String, price: BigDecimal, imageUrl: String)
+
+  implicit val articleReader: Reads[Article] = (
+    (__ \ "name").read[String] and
+      (__ \ "brand" \ "name").read[String] and
+      (__ \ "units" \ (0) \ "price" \"value").read[BigDecimal] and
+      (__ \ "media" \ "images" \ (0) \ "mediumUrl").read[String]
+  )(Article.apply _)
+}
---
+++ app/net/codealchemist/sparks42/catalogue/ZalandoService.scala
@@ -0,0 +1,27 @@
+package net.codealchemist.sparks42.catalogue
+
+import javax.inject.Inject
+import net.codealchemist.sparks42.catalogue.Represenation.Article
+import play.api.libs.concurrent.Execution.Implicits._
+import play.api.libs.json._
+import play.api.libs.ws._
+import scala.concurrent.{Await, Future}
+import scala.concurrent.duration._
+
+class ZalandoService @Inject() (ws: WSClient) {
+  def articles: List[Article] = {
+    val baseUrl = "https://api.zalando.com/articles?page="
+    val requests = for {
+      i <- 1 to 4
+    } yield ws.url(baseUrl + i).get
+
+    val articles = Future.sequence(requests).map(_.toList.flatMap { res =>
+      ((res.json) \ "content").validate[List[Article]] match {
+        case JsSuccess(value, _) => value
+        case err: JsError => List.empty
+      }
+    })
+
+    Await.result(articles, 10 seconds)
+  }
+}
